7c6515708ffb785d0ba3e79e6349fb2f1df13ed1
Federico Ortiz <fede@stuffsandstuffs.coldspring.marian.edu>
2017-06-28 16:01:09 -0400

    Setup submit handler for flick form

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('yay')
+const app = {
+    init: function(formSelector) {
+        document
+            .querySelector(formSelector)
+            .addEventListener('submit', this.handleSubmit)
+    },
+
+    handleSubmit: function(ev) {
+        ev.preventDefault()
+        const f = ev.target
+        console.log(f.flickName.value)
+    },
+}
+
+app.init('form#flick-form')
