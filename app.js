7d66855780c56aaadf6a3cca4ed1bff46cc7a82f
Federico Ortiz <fede@stuffsandstuffs.coldspring.marian.edu>
2017-06-28 16:21:51 -0400

    Use new method definition syntax

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const app = {
 
-    init: function(formSelector) {
+    init(selectors) {
         this.max = 0,
+        this.list = document.querySelector(selectors.listSelector)
 
         document
-            .querySelector(formSelector)
+            .querySelector(selectors.formSelector)
             .addEventListener('submit', this.handleSubmit.bind(this))
     },
 
-    handleSubmit: function(ev) {
+    renderListItem(flick) {
+        const item = document.createElement('li')
+        item.textContent = flick.name
+        return item
+    },
+
+    handleSubmit(ev) {
         ev.preventDefault()
         const f = ev.target
         const flick = {
             id: this.max + 1,
             name: f.flickName.value,
         }
-
-        console.log(flick)
+        
+        const listItem = this.renderListItem(flick)
+        this.list.appendChild(listItem)
         this.max ++
     },
 }
 
-app.init('form#flick-form')
+app.init({
+    formSelector: 'form#flick-form', 
+    listSelector: '#flick-list'
+})
