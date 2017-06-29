18ea4279861aea44b4196039485767f442153b6a
Federico Ortiz <fede@stuffsandstuffs.coldspring.marian.edu>
2017-06-29 16:15:48 -0400

    updated removeFlick

--- app.js
+++ app.js
@@ -1,84 +1,3 @@
-/*const app = {
-
-    init(selectors) {
-        this.flicks = []
-        this.max = 0
-        //this.list = document.querySelector(selectors.listSelector)
-        this.list = document
-            .querySelector(selectors.listSelector)
-        this.template = document
-            .querySelector(selectors.templateSelector)
-
-        document
-            .querySelector(selectors.formSelector)
-            .addEventListener('submit', this.handleSubmitFromForm.bind(this))
-
-        this.load()
-    },
-
-    save() {
-        localStorage
-            .setItem('flicks', JSON.stringify(this.flicks))
-    },
-
-    renderListItem(flick) {
-        //const item = document.createElement('li')
-        //item.textContent = flick.name
-        const item = this.template.cloneNode(true)
-        item.classList.remove('template')
-        item.dataset.id = flick.id
-
-        item
-            .querySelector('.flick-name')
-            .textContent = flick.name
-
-        item
-            .querySelector('button.remove')
-            .addEventListener('click', this.removeFlick.bind(this))
-
-        return item
-    },
-
-    removeFlick(ev) {
-        const listItem = ev.target.closest('.flick')
-        listItem.remove()
-
-        for (let i=0; i < this.flicks.length; i++) {
-            const currentId = this.flicks[i].id.toString()
-            if (listItem.dataset.id === currentId) {
-                this.flicks.splice(i, 1)
-                break;
-            }
-        }
-
-        this.save()
-    },
-
-    //handleSubmit(ev) {
-    //    ev.preventDefault()
-    load() {
-        const dinoJSON = localStorage.getItem('flicks')
-        //const f = ev.target
-        //const flick = {
-        //    id: this.max + 1,
-        //    name: f.flickName.value,
-        //}
-        
-        const listItem = this.renderListItem(flick)
-        this.list.insertBefore(listItem, this.list.firstChild)
-        this.flicks.unshift(flick)
-        this.save()
-        this.max ++
-
-        //remove text from form input
-        ev.target.reset()
-
-        //use this to test if array works
-        console.log(this.flicks)
-    },
-}*/
-
-
 const app = {
    init(selectors) {
      this.flicks = []
@@ -152,7 +71,7 @@ const app = {
 
         item
             .querySelector('button.remove')
-            .addEventListener('click', this.removeFlick.bind(this))
+            .addEventListener('click', this.removeFlick.bind(this, flick))
         item
             .querySelector('button.fav')
             .addEventListener('click', this.favFlick.bind(this, flick))
@@ -214,17 +133,19 @@ const app = {
         this.save()
     },
 
-    removeFlick(ev) {
+    removeFlick(flick, ev) {
         const listItem = ev.target.closest('.flick')
         listItem.remove()
 
-        for (let i=0; i < this.flicks.length; i++) {
+        /*for (let i=0; i < this.flicks.length; i++) {
             const currentId = this.flicks[i].id.toString()
             if (listItem.dataset.id === currentId) {
                 this.flicks.splice(i, 1)
                 break;
             }
-        }
+        }*/
+        const i = this.flicks.indexOf(flick)
+        this.flicks.splice(i, 1)
 
         this.save()
     },
